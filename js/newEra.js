d21a1ad96b8b377282785c8b69e90c0907bd25a5
Agarcia-Hemac <agarcia@grupohemac.com.mx>
2023-03-13 15:18:54 -0600

    Body Work
    Se termino la parte de Body Work
    Y se hicieron cambios en la parte del index
    Se volvio transparente el menu al momento de bajar

--- js/newEra.js
+++ js/newEra.js
@@ -1,10 +1,22 @@
 
 var navbar = document.querySelector('.classNavMenu');
-
+/*
 window.addEventListener('scroll', function() {
   if (window.pageYOffset > 100) {
     navbar.classList.add('transparent');
   } else {
     navbar.classList.remove('transparent');
+  }
+}); */
+
+
+window.addEventListener('scroll', function() {
+  var contenedor = document.querySelector('.classNavMenu');
+  var scrollPosition = window.scrollY;
+  
+  if (scrollPosition > 0) {
+    contenedor.classList.add('transparency');
+  } else {
+    contenedor.classList.remove('transparency');
   }
 });
